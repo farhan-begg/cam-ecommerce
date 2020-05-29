7ca3b6b01c8f644d7ac4dfa46f8fc44c1944e644
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-05-28 17:05:31 -0700

    home screen/ product screen displaying

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
-import data from './data'
+import data from './data';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
 
 function App() {
   const openMenu = () => {
@@ -12,61 +15,65 @@ function App() {
 
 
   return (
-    <div class="grid-container">
-      <header class="header">
-        <div class="brand">
-          <button onClick={openMenu}>
-            &#9776;
+    <BrowserRouter>
+      <div class="grid-container">
+        <header class="header">
+          <div class="brand">
+            <button onClick={openMenu}>
+              &#9776;
             </button>
-          <a href="index.html">Mora</a>
-        </div>
-        <div className="header-links">
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
-        </div>
-      </header>
-      <aside className="sidebar">
-        <h3>Shopping Categories</h3>
-        <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-        <ul>
-          <li>
-            <a href="index.html">Pants</a>
-          </li>
+            <a href="index.html">Mora</a>
+          </div>
+          <div className="header-links">
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+          </div>
+        </header>
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+          <ul>
+            <li>
+              <a href="index.html">Pants</a>
+            </li>
 
-          <li>
-            <a href="index.html">Shirts</a>
-          </li>
-
-        </ul>
-      </aside>
-      <main className="main">
-        <div className="content">
-          <ul className="products">
-            {
-              data.products.map(product =>
-                <li>
-                  <div className="product">
-                    <img className="product-image" src={product.image} alt="product" />
-                    <div className="product-name">
-                      <a href="product.html">{product.name}</a>
-                    </div>
-                    <div className="product-brand">{product.brand}</div>
-                    <div className="product-price">{product.price}</div>
-                    <div className="product-rating">{product.rating}Stars ({product.numReviews})</div>
-                  </div>
-                </li>
-
-              )}
-
+            <li>
+              <a href="index.html">Shirts</a>
+            </li>
 
           </ul>
-        </div>
+        </aside>
+        <main className="main">
+          <div className="content">
+            <Route path="/products/:id" component={ProductScreen} />
+            <Route path="/" exact={true} component={HomeScreen} />
+            <ul className="products">
+              {
+                data.products.map(product =>
+                  <li>
+                    <div className="product">
+                      <img className="product-image" src={product.image} alt="product" />
+                      <div className="product-name">
+                        <a href="product.html">{product.name}</a>
+                      </div>
+                      <div className="product-brand">{product.brand}</div>
+                      <div className="product-price">{product.price}</div>
+                      <div className="product-rating">{product.rating}Stars ({product.numReviews})</div>
+                    </div>
+                  </li>
 
-      </main>
-      <footer className="footer">
-        All right reserved.
+                )}
+
+
+            </ul>
+          </div>
+
+        </main>
+        <footer className="footer">
+          All right reserved.
     </footer>
-    </div>
+      </div>
+    </BrowserRouter>
 
   );
 }
