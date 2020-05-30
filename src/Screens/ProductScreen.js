2e4f04c2f0538d33919995d12f03c4f80f125c7b
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-05-30 12:35:30 -0700

    fixed review

--- src/Screens/ProductScreen.js
+++ src/Screens/ProductScreen.js
@@ -7,7 +7,7 @@ function ProductScreen(props) {
     const product = data.products.find(x => x._id === props.match.params.id);
     return <div>
 
-        <div>
+        <div className="back-to-result">
             <Link to="/">Back to result</Link>
         </div>
         <div className="details">
@@ -23,7 +23,7 @@ function ProductScreen(props) {
                         {product.rating} Stars (product.numReviews) Reviews)
                     </li>
                     <li>
-                        <b>{product.price}</b>
+                        Price <b>${product.price}</b>
                     </li>
                     <li>
                         Description:
@@ -34,6 +34,31 @@ function ProductScreen(props) {
                     </li>
 
                 </ul>
+            </div>
+            <div className="details-action">
+                <ul>
+                    <li>
+                        Price {product.price}
+
+                    </li>
+                    <li>
+                        Status: {product.status}
+                    </li>
+                    <li>
+                        Qty: <select>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                            <option>6</option>
+                        </select>
+                    </li>
+                    <li>
+                        <button className="button">Add to Cart</button>
+                    </li>
+                </ul>
+
             </div>
 
 
