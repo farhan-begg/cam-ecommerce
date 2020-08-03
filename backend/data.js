c4f96a02d575bf1751a741a12c4bfe64a7ff9a31
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-08-03 16:32:37 -0700

    product details

--- backend/data.js
+++ backend/data.js
@@ -6,21 +6,20 @@ export default {
             category: 'Shirts',
             image: '/images/d1.jpg',
             price: 60,
-            brand: 'Nike',
+            brand: ' Nike',
             rating: 4.5,
-            numReviews: 10
-
+            numReviews: 10,
+            countInStock: 6,
         },
         {
             _id: '2',
             name: 'Fit Shirt',
             category: 'Shirts',
             image: '/images/d1.jpg',
-            price: 80,
-            brand: 'Nike',
+            price: 50,
+            brand: ' Nike',
             rating: 4.2,
-            numReviews: 10
-
+            numReviews: 5
         },
         {
             _id: '3',
@@ -28,11 +27,20 @@ export default {
             category: 'Pants',
             image: '/images/d1.jpg',
             price: 70,
-            brand: 'Nike',
+            brand: ' Nike',
             rating: 4.5,
-            numReviews: 10
-
+            numReviews: 8,
+            countInStock: 6,
+        }, {
+            _id: '4',
+            name: 'Best Pants',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 70,
+            brand: ' Nike',
+            rating: 4.5,
+            numReviews: 8,
+            countInStock: 6,
         },
     ]
-
 }
--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,69 +1,90 @@
-import React from 'react';
-import data from '../data';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { detailsProduct } from '../actions/productAction'
 
 function ProductScreen(props) {
-    console.log(props.match.params.id)
-    const product = data.products.find(x => x._id === props.match.params.id);
+
+    const productDetails = useSelector(state => state.productDetails);
+    const { product, loading, error } = productDetails;
+    const dispatch = useDispatch;
+
+    useEffect(() => {
+        dispatch(detailsProduct());
+        return () => {
+        }
+    }, [])
+
     return <div>
 
         <div className="back-to-result">
             <Link to="/">Back to result</Link>
         </div>
-        <div className="details">
-            <div className="details-image">
-                <img src={product.image} alt="product"></img>
-            </div>
-            <div className="details-info">
-                <ul>
-                    <li>
-                        <h4>{product.name}</h4>
+
+        {loading ? <div>Loading...</div> :
+            error ? <div>{error}</div> :
+                (
+                    <div className="details">
+                        <div className="details-image">
+                            <img src={product.image} alt="product"></img>
+                        </div>
+                        <div className="details-info">
+                            <ul>
+                                <li>
+                                    <h4>{product.name}</h4>
+                                </li>
+                                <li>
+                                    {product.rating} Stars (product.numReviews) Reviews)
                     </li>
-                    <li>
-                        {product.rating} Stars (product.numReviews) Reviews)
-                    </li>
-                    <li>
-                        Price <b>${product.price}</b>
-                    </li>
-                    <li>
-                        Description:
+                                <li>
+                                    Price <b>${product.price}</b>
+                                </li>
+                                <li>
+                                    Description:
                         <div>
-                            {product.description}
+                                        {product.description}
+                                    </div>
+
+                                </li>
+
+                            </ul>
+                        </div>
+                        <div className="details-action">
+                            <ul>
+                                <li>
+                                    Price {product.price}
+
+                                </li>
+                                <li>
+                                    Status: {product.status}
+                                </li>
+                                <li>
+                                    Qty: <select>
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                        <option>6</option>
+                                    </select>
+                                </li>
+                                <li>
+                                    <button className="button">Add to Cart</button>
+                                </li>
+                            </ul>
+
                         </div>
 
-                    </li>
 
-                </ul>
-            </div>
-            <div className="details-action">
-                <ul>
-                    <li>
-                        Price {product.price}
 
-                    </li>
-                    <li>
-                        Status: {product.status}
-                    </li>
-                    <li>
-                        Qty: <select>
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                            <option>6</option>
-                        </select>
-                    </li>
-                    <li>
-                        <button className="button">Add to Cart</button>
-                    </li>
-                </ul>
+                    </div>
 
-            </div>
+
+                )
+        }
 
 
 
-        </div>
     </div>
 
 
