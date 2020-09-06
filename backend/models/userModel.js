b847d2c5cf18ff2a98e0daf178241ab99689859a
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-09-05 19:43:09 -0700

    backend user routes

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -6,3 +6,10 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true },
     isAdmin: { type: Boolean, required: true, default: false }
 });
+
+
+
+const userModel = mongoose.model("User", userSchema)
+
+
+export default userModel;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import data from './data';
-import config from './config';
 import dotenv from 'dotenv';
+import config from './config';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import userRoute from './routes/userRoutes';
 
 dotenv.config();
-
-const app = express();
 
 const mongodbUrl = config.MONGODB_URL;
 mongoose.connect(mongodbUrl, {
@@ -15,18 +15,21 @@ mongoose.connect(mongodbUrl, {
     useCreateIndex: true
 }).catch(error => console.log(error.reason));
 
+
+const app = express();
+app.use(bodyParser.json());
+app.use("/api/users", userRoute);
 app.get("/api/products/:id", (req, res) => {
     const productId = req.params.id;
-    const product = data.products.find(x => x._id === productId)
+    const product = data.products.find(x => x._id === productId);
     if (product)
         res.send(product);
     else
-        res.status(404).send({ msg: "Product Not Found" })
+        res.status(404).send({ msg: "Product Not Found." })
 });
 
-app.get("/api/products/", (req, res) => {
-
-    res.send(data.products)
+app.get("/api/products", (req, res) => {
+    res.send(data.products);
 });
 
-app.listen(5000, () => { console.log("Server started at http://localhost:5000") })
+app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
